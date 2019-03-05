fa35869e9ac9b59b21a2a1b0d7ca8ef20d77960e
Jarvis Yang <jarvisgyang@gmail.com>
2019-03-05 14:29:23 -0600

    commented the rules of bonus calculation; added first rule on line 54

--- client.js
+++ client.js
@@ -49,7 +49,23 @@ function bonusCalc( employee ) {
 
   // Calculate bonusPercentage
   let bonusPercentage = 0;
-  
+
+  // Those who have a rating of a 2 or below should not receive a bonus.
+  if (employee.reviewRating <= 2) {
+    bonusPercentage = 0;
+  }
+
+  // Those who have a rating of a 3 should receive a base bonus of 4 % of their base annual income.
+
+  // Those who have a rating of a 4 should receive a base bonus of 6 % of their base annual income.
+
+  // Those who have a rating of a 5 should receive a base bonus of 10 % of their base annual income.
+
+  // If their employee number is 4 digits long, this means they have been with the company for longer than 15 years, and should receive an additional 5 %.
+
+  // However, if their annual income is greater than $65, 000, they should have their bonus adjusted down 1 %.
+
+  // No bonus can be above 13 % or below 0 % total.
 
   // Calculate totalBonus
   let totalBonus = employee.annualSalary * bonusPercentage;
